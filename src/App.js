43da02c9ca51f2ec5d9683eaf9fc9c20ae10f9dc
Zach Nash <afreakenturtle@gmail.com>
2022-05-05 21:35:50 -0400

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+let board = [
+  ['-', '-', '-'],
+  ['-', '-', '-'],
+  ['-', '-', '-']
+]
+
 
 function App() {
+  const [gameBoard, setGameBoard] = useState(board);
+  const [player, setPlayer] = useState('X');
+
+  let resetGame = () => {
+    console.log('reset game');
+    setGameBoard(board)
+  }
+
+  let selectCell = (event) => {
+    console.log(event);
+    let row = event.target.dataset.row;
+    let column = event.target.dataset.column;
+
+    if (gameBoard[row][column] === '-') {
+      let newBoard = gameBoard.map(function (arr) {
+        return arr.slice();
+      });
+      newBoard[row][column] = player;
+
+      setGameBoard(newBoard);
+      setPlayer(player == 'X' ? 'O' : 'X')
+
+    } else {
+      alert('Cant move here, Space is taken!')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tic Tac Toe</h1>
+      <h2>Current Player: {player} </h2>
+      {gameBoard.map((row, rowIndex) => {
+        return <div>
+          {row.map((cell, columnIndex) => {
+            return <button
+              data-row={rowIndex}
+              data-column={columnIndex}
+              onClick={selectCell}>{cell}</button>
+          })}
+        </div>
+      })}
+      <br />
+      <button onClick={resetGame}>New Game</button>
     </div>
   );
 }
